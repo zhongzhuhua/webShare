251d45a5818cdcfab11fe409cbf7dc27497385ae
zhongzhuhua <672986035@qq.com>
2016-04-14 11:24:19 +0800

    grunt config update

--- config/Gruntfile.js
+++ config/Gruntfile.js
@@ -1,8 +1,37 @@
 module.exports = function(grunt) {
+  var mozjpeg = require('imagemin-mozjpeg');
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    // 压缩本地
+    // ============== 文件合并  ===============
+    concat: {
+      // 合并公用 css
+      commonCss: {
+        src: ['assets/lib/layer.mobile/layer/need/layer.css', 'assets/css/global.css', 'assets/css/layout.css'],
+        dest: 'dist/assets/css/common.css'
+      },
+      // 合并插件 js
+      libJs: {
+        src: ['assets/lib/require.js', 'assets/lib/require.config.js', 'assets/lib/ice.js'],
+        dest: 'dist/assets/js/common.js'
+      }
+    },
+
+    // ============== css 压缩  ===============
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      // 压缩公用 css
+      commonCss: {
+        src: 'dist/assets/css/common.css',
+        dest: 'dist/assets/css/common.css'
+      }
+    },
+
+    // ============== js 压缩  ===============
     uglify: {
       options: {
         mangle: {
@@ -14,65 +43,77 @@ module.exports = function(grunt) {
       build: {
         files: [{
           expand: true,
-          cwd: 'assets/js/proj/',
-          src: ['**/*.js'],
-          dest: 'assets/js/proj-min/',
+          cwd: 'dist/assets/js/',
+          src: ['common.js'],
+          dest: 'dist/assets/js/',
           extDot: '',
           ext: '.js'
-        },{
+        }, {
           expand: true,
-          cwd: 'assets/js/models/',
-          src: ['**/*.js'],
-          dest: 'assets/js/lib/models/',
+          cwd: 'assets/js/',
+          src: ['*/*.js'],
+          dest: 'dist/assets/js/',
           extDot: '',
           ext: '.js'
         }]
       }
     },
 
-    // 合并 css
-    concat: {
-      css: {
-        src: ['assets/css/global.css', 'assets/css/layout.css','assets/css/plus.css'],
-        dest: 'assets/css/all.css'
+    // =============== 合并 html =============
+    includereplace: {
+      meta: {
+        options: {
+
+        },
+        src: 'html/**/*.html',
+        dest: 'dist/'
       }
     },
 
-    // 压缩 css
-    cssmin: {
-      options: {
-        shorthandCompacting: false,
-        roundingPrecision: -1
-      },
-      css: {
-        src: 'assets/css/all.css',
-        dest: 'assets/css/all.min.css'
-      }
-    },
-
-    // 打包
+    // =============== 插件复制 =============
     copy: {
       main: {
         files: [{
           expand: true,
-          src: ['html/**'],
-          dest: 'pack/backend'
-        },{
+          src: ['assets/lib/*/**'],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    // ============== html 压缩 =============
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
           expand: true,
-          src: ['assets/js/proj-min/**'],
-          dest: 'pack/backend'
-        },{
+          cwd: 'dist/html',
+          src: '**/*.html',
+          dest: 'dist/html'
+        }]
+      }
+    },
+
+    // ============== 图片压缩 =============
+    imagemin: {
+      dynamic: {
+        options: {
+          //定义 PNG 图片优化水平
+          optimizationLevel: 7,
+          pngquant: true,
+          svgoPlugins: [{
+            removeViewBox: false
+          }],
+          use: [mozjpeg()]
+        },
+        files: [{
           expand: true,
-          src: ['data/**'],
-          dest: 'pack/backend'
-        },{
-          expand: true,
-          src: ['imageAudit/shard-readme.html'],
-          dest: 'pack/backend'
-        },{
-          expand: true,
-          src: ['assets/css/**','assets/js/lib/**'],
-          dest: 'pack/frontend'
+          cwd: 'assets/images',
+          src: ['**/*.{png,jpg,jpeg}'],
+          dest: 'dist/assets/images'
         }]
       }
     }
@@ -81,5 +122,36 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.registerTask('default', ['uglify','concat', 'cssmin', 'copy']);
+  grunt.loadNpmTasks('grunt-include-replace');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+
+  grunt.registerTask('img', '图片压缩', function() {
+    grunt.task.run(['imagemin']);
+  });
+
+  grunt.registerTask('lib', '复制插件', function() {
+    grunt.task.run(['copy']);
+  });
+
+  grunt.registerTask('cssmin', '合并压缩样式', function() {
+    grunt.task.run(['concat', 'cssmin']);
+  });
+
+  grunt.registerTask('jsmin', '合并压缩脚本', function() {
+    grunt.task.run(['concat', 'uglify']);
+  });
+
+  grunt.registerTask('include', '合并页面', function() {
+    grunt.task.run('includereplace');
+  });
+
+  grunt.registerTask('html', '压缩页面', function() {
+    grunt.task.run(['include', 'htmlmin']);
+  });
+
+  grunt.registerTask('default', '默认任务', function() {
+    grunt.task.run(['concat', 'cssmin', 'uglify', 'includereplace'])
+  });
 };
--- config/gulp.js
+++ config/gulp.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 // 压缩 css 文件
 var minifyCss = require('gulp-minify-css');
-// 压缩 html 文件
-var htmlmin = require('gulp-htmlmin');
 // 合并文件，类似母板页
 var contentIncluder = require('gulp-content-includer');
+// 压缩 html 文件
+var htmlmin = require('gulp-htmlmin');
 // 图片压缩
 var imagemin = require('gulp-imagemin');
 // png 压缩
@@ -49,12 +49,23 @@ gulp.task('images', function() {
     .pipe(gulp.dest('./dist/assets/images/'));
 });
 
+// 数据复制
+gulp.task('data', function() {
+  gulp.src('./data/**/*.json')
+    .pipe(gulp.dest('./dist/data/'));
+  gulp.src('./wx/**/*.json')
+    .pipe(gulp.dest('./dist/wx/'));
+});
+
 //定义默认任务
-gulp.task('default', ['css', 'html', 'images']);
+gulp.task('default', ['css', 'html', 'images', 'data']);
 
 // 监控任务
 gulp.task('watch', function() {
-  gulp.watch('./html/*', ['html']);
+  gulp.watch('./html/**/*.html', ['html']);
+  gulp.watch('./modules/*.html', ['html']);
   gulp.watch('./assets/css/*.css', ['css']);
   gulp.watch('./assets/images/*.*', ['images']);
+  gulp.watch('./data/**/*.json', ['data']);
+  gulp.watch('./wx/**/*.json', ['data']);
 });
